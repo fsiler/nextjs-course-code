e1b08bd2c7076410f02ddc166690d8fc684c351f
Frank Siler <me@franksiler.com>
2022-09-25 20:55:03 -0500

    part 80

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,16 @@
+import { Fragment } from "react";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/event-list";
+import EventsSearch from "../../components/events-search";
 
 function EventsPage() {
   const events = getAllEvents();
+
   return (
-    <div>
+    <Fragment>
+      <EventsSearch />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 }
 export default EventsPage;
