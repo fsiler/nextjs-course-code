feb74abb66da3cc74251336e091c53b18f202f22
Frank Siler <me@franksiler.com>
2022-09-25 21:25:33 -0500

    cleaning up but skipping a few stylistic things

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,12 +1,40 @@
 import { useRouter } from "next/router";
+import { Fragment } from "react";
+
+import { getFilteredEvents } from "../../dummy-data";
+
+import Button from "../../components/ui/button";
+import EventList from "../../components/event-list";
 
 function EventPage() {
   const router = useRouter();
-  console.log(router.query);
-  return (
-    <div>
-      <h1>Some event filtered by date and other extended criteria</h1>
-    </div>
-  );
+  const filterData = router.query.slug;
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const year = +filterData[0];
+  const month = +filterData[1];
+  if (
+    isNaN(year) ||
+    isNaN(month) ||
+    month < 1 ||
+    month > 12 ||
+    year < 2020 ||
+    year > 2030
+  ) {
+    return <p>values no good, please try again</p>;
+  }
+  const filteredEvents = getFilteredEvents({ year: year, month: month });
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <p>no events found</p>
+        <Button link="/events">All Events</Button>
+      </Fragment>
+    );
+  }
+
+  return <EventList items={filteredEvents} />;
 }
 export default EventPage;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,20 @@
 import { Fragment } from "react";
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/event-list";
 import EventsSearch from "../../components/events-search";
 
 function EventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    router.push(`/events/${year}/${month}`);
+  }
 
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   );
